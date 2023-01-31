0a6b825b1555eae689beed47ea379959e95e6c65
Minkyu.kim <minkyu.kim@bagelcode.com>
2023-01-31 20:33:14 +0900

    Detached Server and Shaped Start Flow

--- Assets/ProjectA/Scripts/GamePlay/Addon/Attack/AttackBase.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class AttackBase : EnvLayer
-{
-    public abstract void Attack(UnitBase sender, UnitBase target, int damage, string dialog);
-}
--- Assets/ProjectA/Scripts/GamePlay/Addon/Move/MoveBase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class MoveBase : EnvLayer
-{
-    public abstract void Move(UnitBase sender);
-
-    public abstract void Stop(UnitBase sender);
-}
