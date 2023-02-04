288b90ff9f32feda1be34eee7582b1fae93b4adc
haeinsa <wh4014@naver.com>
2023-02-04 21:08:05 +0900

    Delete wrongly imported plugins

--- Assets/amazon-gamelift-plugin-unity-1.2.1/Samples~/SampleGame/Assets/Scripts/Strings.cs
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
-public static class Strings
-{
-    public const string ErrorUnknown = "An error occured. Please try again later.";
-    public const string ErrorStartingGame = "Failed to find a game. Please try again later.";
-}
--- Assets/amazon-gamelift-plugin-unity-1.2.1/Scripts~/windows/release.ps1
+++
@@ -1,3 +0,0 @@
-& "$PSScriptRoot\clean.ps1"
-& "$PSScriptRoot\build.ps1"
-& "$PSScriptRoot\export.ps1"
