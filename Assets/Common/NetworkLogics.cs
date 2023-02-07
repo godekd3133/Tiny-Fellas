ede2837df7de7b736eb0efd864e040958028ce64
seungmin_cho <seungmin.cho@bagelcode.com>
2023-02-07 18:00:08 +0900

    fix compile error

--- Assets/Common/NetworkLogics.cs
+++ Assets/Common/NetworkLogics.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 public class NetworkLogics 
 {
@@ -42,19 +44,8 @@ public class NetworkLogics
 
     public static string[] ReceiveUDP(UdpClient client)
     {
-        NetworkStream stream = client.GetStream();
         var messages = new List<string>();
 
-        while (stream.DataAvailable)
-        {
-            byte[] bufferLength = new byte[4];
-            stream.Read(bufferLength, 0, bufferLength.Length);
-            int msgSize = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(bufferLength, 0));
-            byte[] readBuffer = new byte[msgSize];
-            stream.Read(readBuffer, 0, readBuffer.Length);
-            string msgStr = Encoding.ASCII.GetString(readBuffer, 0, readBuffer.Length);
-            messages.Add(msgStr);
-        }
 
         return messages.ToArray();
     }
@@ -62,11 +53,8 @@ public class NetworkLogics
     public static void SendUDP(UdpClient client, string msgStr)
     {
 
-        NetworkStream stream = client.GetStream();
         byte[] writeBuffer = Encoding.ASCII.GetBytes(msgStr);
         int msgSize = writeBuffer.Length;
         byte[] bufferLength = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(msgSize));
-        stream.Write(bufferLength, 0, bufferLength.Length);
-        stream.Write(writeBuffer, 0, msgSize);
     }
 }
