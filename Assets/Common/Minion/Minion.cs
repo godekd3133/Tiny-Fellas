b23b87c22f29527c6654c3ca75248af7972e2bd5
seungmin_cho <seungmin.cho@bagelcode.com>
2023-02-09 17:25:38 +0900

    [Minion]  add member for stat

--- Assets/Common/Minion/Minion.cs
+++ Assets/Common/Minion/Minion.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(MinionInstanceStat))]
 public class Minion : MonoBehaviour
 {
     public PlayerData ownerPlayer;
     public float moveSpeed;
     public NavMeshAgent agent;
+    public MinionInstanceStat stat;
+    
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
