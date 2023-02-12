f12974232004aac9602036f8ac9bf44c3cfd7c26
Minkyu.kim <minkyu.kim@bagelcode.com>
2023-02-12 23:29:10 +0900

    Refactoried Scripts

--- Assets/Client/Scripts/Common/Singleton.cs
+++ Assets/Client/Scripts/Common/Singleton.cs
@@ -19,15 +19,15 @@ public class Singleton<T> where T : new()
 
 public class SingletonPerScene<T> where T : new()
 {
-    private static Dictionary<Scene,T> instanceMap = new Dictionary<Scene, T>();
+    private static Dictionary<Scene, T> instanceMap = new Dictionary<Scene, T>();
 
     public static T GetInstanceOfScene(Scene scene)
     {
-        if(instanceMap.ContainsKey(scene) == false ) instanceMap.Add(scene, new T());
+        if (instanceMap.ContainsKey(scene) == false) instanceMap.Add(scene, new T());
         else if (instanceMap[scene] == null) instanceMap[scene] = new T();
         return instanceMap[scene];
     }
-    
+
     public static T GetInstanceOfScene(GameObject obj)
     {
         var scene = obj.scene;
