a3deb1aa3d28e5031f85a050c3edaab4d0dc0bc5
Min Kyu Kim <minkyu.kim@bagelcode.com>
2023-02-13 14:00:26 +0900

    Refactoried

--- Assets/Client/Scripts/Client/Contents/Minion/Attack/Skills/SkillPassiveBase.cs
+++ Assets/Client/Scripts/Client/Contents/Minion/Attack/Skills/SkillPassiveBase.cs
@@ -1,43 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Castle.Components.DictionaryAdapter.Xml;
 using UnityEngine;
 
-public abstract class ASkillPassiveBase 
+public abstract class ASkillPassiveBase
 {
     // subscribe this function to Minion's callback members
     public void ApplyEffect(Minion minion)
     {
-        minion.SubscribeBeforeDamaged(BeforeDamagedEffect);
-        minion.SubscribeBeforeAttack(BeforeAttackEffect);
-        minion.SubscribeAfterDamaged(AffterDamagedEffect);
-        minion.SubscribeAfterAttack(AfterAttackEffect);
+        minion.befroeDamaged.AddListener(BeforeDamagedEffect);
+        minion.beforeAttack.AddListener(BeforeAttackEffect);
+        minion.afterDamaged.AddListener(AffterDamagedEffect);
+        minion.afterAttack.AddListener(AfterAttackEffect);
     }
 
     public void DettachEffect(Minion minion)
     {
-        minion.UnSubscribeBeforeDamaged(BeforeDamagedEffect);
-        minion.UnSubscribeBeforeAttack(BeforeAttackEffect);
-        minion.UnSubscribeAfterDamaged(AffterDamagedEffect);
-        minion.UnSubscribeAfterAttack(AfterAttackEffect);
+        minion.befroeDamaged.RemoveListener(BeforeDamagedEffect);
+        minion.beforeAttack.RemoveListener(BeforeAttackEffect);
+        minion.afterDamaged.RemoveListener(AffterDamagedEffect);
+        minion.afterAttack.RemoveListener(AfterAttackEffect);
     }
 
     protected virtual void BeforeAttackEffect(Minion minion)
     {
-        
+
     }
-    
+
     protected virtual void AfterAttackEffect(Minion minion)
     {
-        
+
     }
-    
+
     protected virtual void BeforeDamagedEffect(Minion minion)
     {
-        
+
     }
-    
+
     protected virtual void AffterDamagedEffect(Minion minion)
     {
-        
+
     }
 }
