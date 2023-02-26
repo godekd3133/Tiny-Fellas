9cc935376e8fc9ae6f9d725c49683c4b02c558ec
haeinsa <wh4014@naver.com>
2023-02-26 22:37:00 +0900

    [GameSessionInstance] now PlayerData list is network variable, means it alwyas is replicated

--- Assets/Client/Scripts/Server/GameSessionInstance.cs
+++ Assets/Client/Scripts/Server/GameSessionInstance.cs
@@ -10,21 +10,23 @@ using Unity.Netcode;
 
 // empty classs for inherit NetworkManager
 
-public class GameSessionInstance : MonoWeakSingleton<GameSessionInstance>
+public class GameSessionInstance : NetworkBehaviourSingleton<GameSessionInstance>
     #if UNITY_SERVER || UNITY_EDITOR
     ,IMinionDeployable
     #endif
 {
     [SerializeField] private bool isLocalTest;
     
-    private List<PlayerData> playerDataList;
+    private NetworkVariable<List<PlayerData>> playerDataList;
     private Dictionary<string, PlayerData> playerDataByPlayerID ;
     private List<MinionData> minionDeck;
     private List<Minion> minionInstanceList;
     private GameSession gameSession;
 
+#if UNITY_SERVER || UNITY_EDITOR
     public IReadOnlyList<MinionData> MinionDeck => minionDeck;
     public IReadOnlyList<Minion> MinionInstanceList => minionInstanceList;
+#endif
     public IReadOnlyList<PlayerData> PlayerDataList => playerDataList;
 
     public IReadOnlyDictionary<string, PlayerData> PlayerDataByPlayerID
@@ -34,7 +36,7 @@ public class GameSessionInstance : MonoWeakSingleton<GameSessionInstance>
             if (playerDataByPlayerID == null)
             {
                 playerDataByPlayerID = new Dictionary<string, PlayerData>();
-                foreach (var playerData in playerDataList)
+                foreach (var playerData in playerDataList.Value)
                     playerDataByPlayerID.Add(playerData.PlayerSession.PlayerId, playerData);
             }
 
